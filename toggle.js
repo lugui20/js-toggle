56cfd77c1d986fd4bc448dfa8887cd68febfe1ad
Luis Uhlig <lugui20@protonmail.com>
2023-09-15 16:03:11 -0300

    First commit

---
+++ toggle.js
@@ -0,0 +1,9 @@
+function toggleOn() {
+    document.getElementById("toggle-on").style.display = "block";
+    document.getElementById("toggle-off").style.display = "none";
+}
+
+function toggleOff() {
+    document.getElementById("toggle-on").style.display = "none";
+    document.getElementById("toggle-off").style.display = "block";
+}
